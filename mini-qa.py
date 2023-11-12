8f95d6a1d2b0c627cbbb8d52a6becb853e739307
Arto Päivinen <arto.paeivinen@gmail.com>
2023-11-12 22:44:55 +0200

    Final functioning solution.

--- mini-qa.py
+++ mini-qa.py
@@ -1,11 +1,11 @@
 import config                                                       # My own keys hiding there ..
 
-ASTRA_DB_SECURE_BUNDLE_PATH = config.MyAstra_DB_secure_bundle_path  #"<<PATH_TO_YOUR_SECURE_BUNDLE>>"
-ASTRA_DB_APPLICATION_TOKEN = config.MyAstra_DB_app_token            #"<<YOUR_TOKEN_VALUE>>"       #"AstraCD:..."
-ASTRA_DB_CLIENT_ID = config.MyAstra_DB_client_ID                    #"token"
+ASTRA_DB_SECURE_BUNDLE_PATH = config.MyAstra_DB_secure_bundle_path  # "<<PATH_TO_YOUR_SECURE_BUNDLE>>"
+ASTRA_DB_APPLICATION_TOKEN = config.MyAstra_DB_app_token            # "<<YOUR_TOKEN_VALUE>>"       #"AstraCD:..."
+ASTRA_DB_CLIENT_ID = config.MyAstra_DB_client_ID                    # "token"
 ASTRA_DB_CLIENT_SECRET = config.MyAstra_DB_client_secret
-ASTRA_DB_KEYSPACE = config.MyAstra_DB_Keyspace                      #"<<YOUR_KEYSPACE_NAME>>"
-OPENAI_API_KEY = config.MyOpenAPIkey                                #"<<YOUR_OPENAI_KEY>>"
+ASTRA_DB_KEYSPACE = config.MyAstra_DB_Keyspace                      # "<<YOUR_KEYSPACE_NAME>>"
+OPENAI_API_KEY = config.MyOpenAPIkey                                # "<<YOUR_OPENAI_KEY>>"
 
 from langchain.vectorstores.cassandra import Cassandra
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
@@ -42,3 +42,24 @@ print("\nGenerating embeddings and storing in AstraDB")
 myCassandraVStore.add_texts(headlines)
 
 print("inserted %i headlines.\n" % len(headlines))
+
+vectorIndex = VectorStoreIndexWrapper(vectorstore=myCassandraVStore)
+
+first_question = True
+while True:
+    if first_question:
+        query_text = input("\nEnter your question (or type 'quit' to exit): ")
+        first_question = False
+    else:
+        query_text = input("\nWhat's your next question? (or type 'quit' to exit): ")
+
+    if query_text.lower() == 'quit':
+        break
+
+    print("QUESTION: \"%s\"" % query_text)
+    answer = vectorIndex.query(query_text, llm=llm).strip()
+    print("ANSWER: \"%s\"\n" % answer)
+
+    print("DOCUMENTS BY RELEVANCE:")
+    for doc, score in myCassandraVStore.similarity_search_with_score(query_text, k=4):
+        print("   %0.4f \"%s ... \"" % (score, doc.page_content[:60]))
